2ff0edb4d5062cb45e5a44bef8a076f822682f15
M-Goldman <macalleegoldman7@gmail.com>
2019-12-01 01:36:25 -0500

    Added Propagation Feature, Refactored TS and SCSS code

--- src/assets/app.ts
+++ src/assets/app.ts
@@ -1,16 +1,16 @@
 // Initialize Bindings
 
-const blackBtn = $("#blackBtn");
-const rainbowBtn = $("#rainbowBtn");
-const transparentBtn = $("#transparentBtn");
-const lightGradientBtn = $("#lightGradientBtn");
-const darkGradientBtn = $("#darkGradientBtn");
-const colorBtns = $(".color");
-const clearBtn = $("#clearBtn");
-const newGridBtn = $("#newGridBtn");
-const propagateBtn = $("#propagatebtn");
-let container: any = $(".container");
-let boxes: JQuery<HTMLElement>;
+const blackBtn: JQuery < HTMLElement > = $("#blackBtn");
+const rainbowBtn: JQuery < HTMLElement > = $("#rainbowBtn");
+const transparentBtn: JQuery < HTMLElement > = $("#transparentBtn");
+const lightGradientBtn: JQuery < HTMLElement > = $("#lightGradientBtn");
+const darkGradientBtn: JQuery < HTMLElement > = $("#darkGradientBtn");
+const colorBtns: JQuery < HTMLElement > = $(".color");
+const clearBtn: JQuery < HTMLElement > = $("#clearBtn");
+const newGridBtn: JQuery < HTMLElement > = $("#newGridBtn");
+const propagateBtn: JQuery < HTMLElement > = $("#propagateBtn");
+let container: JQuery < HTMLElement > = $(".container");
+let boxes: JQuery < HTMLElement > ;
 
 // Initialize Initial State
 
@@ -18,7 +18,7 @@ populateGrid(16);
 
 const selections: any = {
     color: "black",
-    propagate: false,
+    propagate: "off",
 };
 
 // Initialize Event Listeners
@@ -30,7 +30,7 @@ lightGradientBtn.click(() => selections.color = "Light Gradient");
 darkGradientBtn.click(() => selections.color = "Dark Gradient");
 clearBtn.click(clearGrid);
 newGridBtn.click(newGrid);
-propagateBtn.click(() => selections.propagate === true ? selections.propagate = false : selections.propagate = true);
+propagateBtn.click(propagateToggle);
 
 // Initialize Function Logic
 
@@ -38,8 +38,9 @@ function populateGrid(gridsize: number) {
     container.attr("style", `grid-template: repeat(${gridsize}, 1fr) / repeat(${gridsize}, 1fr);`);
     for (let i: number = 1; i <= gridsize; i++) {
         for (let j: number = 1; j <= gridsize; j++) {
-            const box: any = $(`<div></div>`);
+            const box: JQuery < HTMLElement > = $(`<div></div>`);
             box.addClass(`box x${j}y${i}`);
+            box.on("transitionend webkitTransitionEnd oTransitionEnd", propagate);
             box.attr("style", "background-color: white;");
             container.append(box);
         }
@@ -48,28 +49,32 @@ function populateGrid(gridsize: number) {
     $(".box").on("mouseenter", mouseOver);
 }
 
-function mouseOver(this: any) {
-
+function mouseOver(this: any, prop: any) {
     switch (selections.color) {
         case "black":
-        $(this).css({backgroundColor: "black", borderColor: "#44c2fc"});
-        break;
+            $(this).css({backgroundColor: "black", borderColor: "#44c2fc"});
+            $(prop).css({backgroundColor: "black", borderColor: "#44c2fc"});
+            break;
 
         case "rainbow":
-        $(this).css({backgroundColor: randomColor().generate, borderColor: "#44c2fc"});
-        break;
+            randomColor($(this));
+            randomColor(prop);
+            break;
 
         case "transparent":
-        $(this).css({backgroundColor: "transparent", borderColor: "transparent"});
-        break;
+            $(this).css({backgroundColor: "transparent", borderColor: "transparent"});
+            $(prop).css({backgroundColor: "transparent", borderColor: "transparent"});
+            break;
 
         case "Light Gradient":
-        changeGradient($(this), -51);
-        break;
+            changeGradient($(this), -51);
+            changeGradient($(prop), -51);
+            break;
 
         case "Dark Gradient":
-        changeGradient($(this), 51);
-        break;
+            changeGradient($(this), 51);
+            changeGradient($(prop), 51);
+            break;
     }
 }
 
@@ -92,24 +97,51 @@ function newGrid() {
     }
 }
 
-function randomColor() {
+function randomColor(self: JQuery < HTMLElement >) {
     const r: number = Math.floor(Math.random() * 255);
     const g: number = Math.floor(Math.random() * 255);
     const b: number = Math.floor(Math.random() * 255);
     const generate: string = `rgb(${r}, ${g}, ${b})`;
-    return {
-        generate,
-    };
+    $(self).css({backgroundColor: generate, borderColor: "#44c2fc"});
 }
 
-function changeGradient(self: any, changeAmount: number) {
+function changeGradient(self: JQuery < HTMLElement >, changeAmount: number) {
     const color: any = $(self).css("backgroundColor").split(",");
-    let r = color[0].split("(")[color[0].split("(").length - 1];
-    let g = color[1];
-    let b = color[2].split(")")[color[1].split(")").length - 1];
+    let r: number = color[0].split("(")[color[0].split("(").length - 1];
+    let g: number = color[1];
+    let b: number = color[2].split(")")[color[1].split(")").length - 1];
 
     r -= changeAmount;
     g -= changeAmount;
     b -= changeAmount;
     $(self).css({backgroundColor: `rgb(${r}, ${g}, ${b})`, borderColor: "#44c2fc"});
+}
+
+function propagate(this: any) {
+    if (selections.propagate === "on") {
+        // tslint:disable-next-line: radix
+        const thisY: number = parseInt(this.classList[1].split("y")[1]);
+        // tslint:disable-next-line: radix
+        const thisX: number = parseInt(this.classList[1].split("y")[0].split("x")[1]);
+        const maxSize: number = Math.sqrt(container.children().length);
+
+        for (let i: number = -1; i <= 1; i++) {
+            for (let j: number = -1; j <= 1; j++) {
+                const tempX: number = thisX + i;
+                const tempY: number = thisY + j;
+                if ((i === 0 && j !== 0) || (i !== 0 && j === 0)) {
+                    if (tempX >= 1 && tempX <= maxSize && tempY >= 1 && tempY <= maxSize) {
+                        const propElement: JQuery < HTMLElement > = $(".x" + tempX + "y" + tempY);
+                        mouseOver(propElement);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function propagateToggle() {
+    selections.propagate === "off" ? selections.propagate = "on" : selections.propagate = "off";
+    // tslint:disable-next-line: max-line-length
+    selections.propagate === "off" ? propagateBtn.css("backgroundColor", "darkred") : propagateBtn.css("backgroundColor", "darkgreen");
 }
